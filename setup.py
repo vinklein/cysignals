0c59203a8f1290a092d9980a3a7e0fb1766499db
vinklein <vincent.klein@u-bordeaux.fr>
2018-02-12 15:40:55 +0100

    cysignals for windows : clean setup.py code

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ kwds = dict(include_dirs=[opj("src", "cysignals"),
 
 extensions = [
     Extension("cysignals.signals", ["src/cysignals/signals.pyx"], **kwds),
+    Extension("cysignals.tests", ["src/cysignals/tests.pyx"], **kwds)
 ]
 
 if os.name is not 'nt':
@@ -58,7 +59,6 @@ if os.name is not 'nt':
         Extension("cysignals.pysignals", ["src/cysignals/pysignals.pyx"], **kwds),
         Extension("cysignals.alarm", ["src/cysignals/alarm.pyx"], **kwds),
         Extension("cysignals.pselect", ["src/cysignals/pselect.pyx"], **kwds),
-        Extension("cysignals.tests", ["src/cysignals/tests.pyx"], **kwds)
     ])
 
 classifiers = [
